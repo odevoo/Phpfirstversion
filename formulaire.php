9df776fa220e83146aebf6db11eb1d43c92eb7bd
odevoo <elandry@odevoo.com>
2016-10-26 07:38:22 +0200

    indent

--- formulaire.php
+++ formulaire.php
@@ -1,32 +1,32 @@
 <p>
-    Cette page ne contient que du HTML.<br />
-    Veuillez taper votre prénom :
+  Cette page ne contient que du HTML.<br />
+  Veuillez taper votre prénom :
 </p>
 
 <form action="cible.php" method="post">
-<p>
+  <p>
     <input type="text" name="prenom" />
 
     <textarea name="message" rows="8" cols="45">
-Votre message ici.
-</textarea>
-<select name="choix">
-    <option value="choix1">Choix 1</option>
-    <option value="choix2">Choix 2</option>
-    <option value="choix3">Choix 3</option>
-    <option value="choix4">Choix 4</option>
-</select>
-<input type="checkbox" name="case" id="case" /> <label for="case">Ma case à cocher</label>
-  <input type="submit" value="Valider" />
-</p>
+      Votre message ici.
+    </textarea>
+    <select name="choix">
+      <option value="choix1">Choix 1</option>
+      <option value="choix2">Choix 2</option>
+      <option value="choix3">Choix 3</option>
+      <option value="choix4">Choix 4</option>
+    </select>
+    <input type="checkbox" name="case" id="case" /> <label for="case">Ma case à cocher</label>
+    <input type="submit" value="Valider" />
+  </p>
 </form>
 
-  <form action="cible_envoi.php" method="post" enctype="multipart/form-data">
-      <p>
-              Formulaire d'envoi de fichier :<br />
-              <input type="file" name="monfichier" /><br />
-              <input type="submit" value="Envoyer le fichier" />
-      </p>
+<form action="cible_envoi.php" method="post" enctype="multipart/form-data">
+  <p>
+    Formulaire d'envoi de fichier :<br />
+    <input type="file" name="monfichier" /><br />
+    <input type="submit" value="Envoyer le fichier" />
+  </p>
 </form>
 <br>
 <br>
